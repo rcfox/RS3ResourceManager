94183aa9146f53f39d0e5556104e7f9268ce913e
Ryan Fox <ryan@rcfox.ca>
2021-05-03 15:26:05 -0400

    Add options to the mod for saving/loading from the filesystem.

--- RS3ResourceManager/Class1.cs
+++ RS3ResourceManager/Class1.cs
@@ -8,16 +8,48 @@ using System;
 
 namespace RS3ResourceManager
 {
+    public class Settings : UnityModManager.ModSettings, IDrawable
+    {
+        [Draw("Extract files as they load")] public bool SaveMissingFiles = false;        
+        [Draw("Load files from the filesystem (if they exist)")] public bool LoadFiles = true;
+        public string LoadFilesOnStartup = "";
+
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            Save(this, modEntry);
+        }
+
+        public void OnChange()
+        {
+            return;
+        }
+    }
+
     static class Main
     {
-        static UnityModManager.ModEntry mod = null;
+        static UnityModManager.ModEntry mod;
+        static Settings settings;
 
         static void Load(UnityModManager.ModEntry modEntry)
         {
             mod = modEntry;
+            settings = Settings.Load<Settings>(modEntry);
 
             var harmony = new Harmony(modEntry.Info.Id);
             harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            modEntry.OnSaveGUI = OnSaveGUI;
+            modEntry.OnGUI = OnGUI;
+        }
+
+        static void OnSaveGUI(UnityModManager.ModEntry modEntry)
+        {
+            settings.Save(modEntry);
+        }
+
+        static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+            settings.Draw(modEntry);
         }
 
         [HarmonyPatch(typeof(Util))]
@@ -31,11 +63,15 @@ namespace RS3ResourceManager
                     return;
                 }
 
-                string fileName = Path.Combine(Path.Combine(mod.Path, "falloc"), fname);
+                string fileName = Path.Combine(Path.Combine(mod.Path, "resources"), fname);
                 try
                 {
                     if (!File.Exists(fileName))
                     {
+                        if (!settings.SaveMissingFiles)
+                        {
+                            return;
+                        }
                         mod.Logger.Log(fileName);
                         Directory.CreateDirectory(Path.GetDirectoryName(fileName));
                         using (BinaryWriter binWriter = new BinaryWriter(File.Open(fileName, FileMode.Create)))
@@ -44,7 +80,10 @@ namespace RS3ResourceManager
                         }
                     } else
                     {
-                        __result = File.ReadAllBytes(fileName);
+                        if (settings.LoadFiles)
+                        {
+                            __result = File.ReadAllBytes(fileName);
+                        }
                     }
                 }
                 catch (Exception e)
@@ -64,11 +103,15 @@ namespace RS3ResourceManager
                 {
                     return;
                 }
-                string fileName = Path.Combine(Path.Combine(mod.Path, "fopen"), fname);
+                string fileName = Path.Combine(Path.Combine(mod.Path, "resources"), fname);
                 try
                 {
                     if (!File.Exists(fileName))
                     {
+                        if (!settings.SaveMissingFiles)
+                        {
+                            return;
+                        }
                         mod.Logger.Log(fileName);
                         Directory.CreateDirectory(Path.GetDirectoryName(fileName));
                         using (BinaryWriter binWriter = new BinaryWriter(File.Open(fileName, FileMode.Create)))
@@ -83,12 +126,29 @@ namespace RS3ResourceManager
                         __result.Seek(0, SeekOrigin.Begin);
                     } else
                     {
-                        __result = File.Open(fileName, FileMode.Open);
+                        if (settings.LoadFiles)
+                        {
+                            __result = File.Open(fileName, FileMode.Open);
+                        }
                     }
                 }
                 catch (Exception e)
                 {
                     mod.Logger.Error(fileName + " -- " + e.ToString());
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(GameMain))]
+        [HarmonyPatch("Start")]
+        static class GameMain_Start_Patch
+        {
+            static void Postfix()
+            {
+                string[] fileNames = settings.LoadFilesOnStartup.Split(',');
+                foreach (string fileName in fileNames)
+                {
+                    Util.falloc(fileName);
                 }
             }
         }
